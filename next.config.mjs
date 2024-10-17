d1be7d0f25f644326799b9383d38c5778007889f
Jesse Rodriguez <wellpeng97@gmail.com>
2024-10-17 20:31:29 +0800

    feat: add docs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,13 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'webresource.c-ctrip.com',
+      port: '',
+    }]
+  }
 };
 
 export default withMDX(config);
